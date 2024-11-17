efa1d20d351c0ac20721f44dc2f5bafe3d1b63fa
IlyaPersh <pershinov@gmail.com>
2024-11-17 22:15:04 +0500

    Задача с прямоугольниками исправленная со со структурами. Отдельный файл с решением мапами

--- 2_carpet.go
+++ 2_carpet.go
@@ -7,71 +7,77 @@ import (
 )
 
 // даны координаты точек(x, y) и их количество, необходимо вычислить максимальную площадь прямоугольника
-//стороны параллельны осям координат
-//находим среди всех возможных отрезков, те которые параллельны оси Х, затем сравниваем их длины и координаты
+// стороны параллельны осям координат
+// находим среди всех возможных отрезков, те которые параллельны оси Х, затем сравниваем их длины и координаты
+type point struct {
+	x int
+	y int
+}
 
 type segment struct { // структура для отрезков параллельных оси Х
-	dist int
-	x    int
-	y1   int
-	y2   int
+	point1 point
+	point2 point
+	dist   int
 }
 
 func main() {
 	var count int
 	r := bufio.NewReader(os.Stdin)
 	fmt.Fscan(r, &count)
-	x := make([]int, count)
-	y := make([]int, count)
-	for i := 0; i < count; i++ {
-		fmt.Fscan(r, &x[i]) //считываем координаты в два слайса X и Y
-		fmt.Fscan(r, &y[i])
-	}
-
+	// x := make([]int, count)
+	// y := make([]int, count)
+	points := make([]point, 0, count)
 	var segments []*segment // срез структур отрезков
+	var maxRectangle int
+
+	// TODO
+	// points := make(map[int][]point) // map x -> point
+	// segments := make(map[int][]segment) // map dist -> segment
+
 	for i := 0; i < count; i++ {
-		for j := i + 1; j < count; j++ { // перебираем все возможные отрезки среди точек
-			if x[i] == x[j] { // если х двух точек равный, то значит отрезок параллелен оси y
-				dist := y[i] - y[j] // считаем расстояние между точками
+		var p point
+		fmt.Fscan(r, &p.x) //считываем координаты в два слайса X и Y
+		fmt.Fscan(r, &p.y)
+
+		for _, pointx := range points { // перебираем все возможные отрезки среди точек
+			// условие, в котором раскрывается ключ к мапе
+			if pointx.x == p.x { // если х двух точек равный, то значит отрезок параллелен оси y
+				dist := pointx.y - p.y // считаем расстояние между точками
 				if dist < 0 {
 					dist *= -1
 				}
-				s := new(segment)
-				s.dist = dist
-				s.x = x[i]
-				s.y1 = y[i]
-				s.y2 = y[j]
+
+				s := &segment{
+					dist:   dist,
+					point1: p,
+					point2: pointx,
+				}
+
+				for _, seg := range segments { // сравниваем отрезки между собой
+					maxRectangle = max(maxRectangle, rectangleArea(s, seg))
+				}
+
 				segments = append(segments, s) //записываем параметры отрезка в структуру, а структуру в слайс
 			}
 		}
 
-	}
+		points = append(points, p)
 
-	maxRectangle := 0
-	for i := 0; i < len(segments); i++ {
-		for j := i + 1; j < len(segments); j++ { // сравниваем отрезки между собой
-			area := rectangleArea(segments[i], segments[j])
-			if area == 0 {
-				continue
-			}
-			if area > maxRectangle {
-				maxRectangle = area //находим максимальную площадь среди возможных прямоугольников
-			}
-
-		}
-
+		// TODO
+		// points[p.x] = append(points[p.x], p)
 	}
 
 	fmt.Println(maxRectangle)
 }
 
 func rectangleArea(a, b *segment) int { //функция проверки отрезков являются ли они сторонами прямоугольника
+	// условие, в котором раскрывается ключ к мапе
 	if a.dist != b.dist {
 		return 0
 	}
 	area := 0
-	if (a.y1 == b.y1 && a.y2 == b.y2) || (a.y1 == b.y2 && a.y2 == b.y1) {
-		area = a.dist * (a.x - b.x) // и расчет площади
+	if a.point1.y == b.point1.y && a.point2.y == b.point2.y || a.point1.y == b.point2.y && a.point2.y == b.point1.y {
+		area = a.dist * (a.point1.x - b.point1.x) // и расчет площади
 		if area < 0 {
 			area *= -1
 		}
